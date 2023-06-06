2396ae7f56a9510dda81d5bcb6bd5299d35d5292
steve zhang <871506263@qq.com>
2023-06-06 13:35:04 +0800

    zhangjinhui提交代码于: 2023-06-06 13:35:04

--- demo/remoteGitTags.mjs
+++ demo/remoteGitTags.mjs
@@ -10,13 +10,15 @@
 // import remoteGitTags from 'remote-git-tags';
 // console.log('remoteGitTags', remoteGitTags)
 
-import { promisify } from 'node:util';
-import childProcess from 'node:child_process';
+import { promisify } from 'node:util'; // promisify函数
+import childProcess from 'node:child_process'; // childProcess对象
+import fs from 'fs';
+
 // console.log('promisify:\n', promisify.toString());
 // console.log('childProcess:\n', childProcess);
 // console.log('childProcess:\n', childProcess.execFile.toString());
 
-const execFile = promisify(childProcess.execFile);
+const execFile = promisify(childProcess.execFile); // execFile函数
 
 export default async function remoteGitTags(repoUrl) {
   const res = await execFile('git', ['ls-remote', '--tags', repoUrl]);
@@ -25,15 +27,15 @@ export default async function remoteGitTags(repoUrl) {
   const tags = new Map();
 
   for (const line of stdout.trim().split('\n')) {
-    console.log('line', line)
+    console.log('line', line);
     const [hash, tagReference] = line.split('\t');
-    console.log('hash', hash)
-    console.log('tagReference', tagReference)
+    console.log('hash', hash);
+    console.log('tagReference', tagReference);
     // Strip off the indicator of dereferenced tags so we can override the
     // previous entry which points at the tag hash and not the commit hash
     // `refs/tags/v9.6.0^{}` → `v9.6.0`
     const tagName = tagReference.replace(/^refs\/tags\//, '').replace(/\^{}$/, '');
-    console.log('tagName', tagName)
+    console.log('tagName', tagName);
     tags.set(tagName, hash);
   }
 
@@ -41,4 +43,14 @@ export default async function remoteGitTags(repoUrl) {
 }
 
 const res = await remoteGitTags('https://github.com/x-extends/vxe-table-plugin-charts');
-console.log('res:', res);
+console.log('res:\n', res);
+
+// Convert the Map object to Array and then convert to a JSON string
+const jsonString = JSON.stringify(Array.from(res), null, '\t');
+console.log('jsonString:\n', jsonString);
+
+// Write the JSON string to a file
+fs.writeFile(`myMap-${Date.now()}.json`, jsonString, err => {
+  if (err) throw err;
+  console.log('Map saved to file!');
+});
